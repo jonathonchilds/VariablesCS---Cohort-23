bcc130f571c0880fc2fffdcb95ec2df7f46c97b3
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2021-11-23 22:04:04 -0500

    Adding spacing/formatting for aesthetic value

--- Program.cs
+++ Program.cs
@@ -6,44 +6,54 @@ namespace VariablesCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C#");
             Console.WriteLine();
+            
             var numberOfCupsOfCoffee = 5;
-            var fullName = "JonathonChilds";
+            var fullName = "Jonathon Childs";
             var today = DateTime.Now.ToString("M/d/yyyy");
-            Console.WriteLine("{0} {1} {2}", numberOfCupsOfCoffee, fullName, today);
-            Console.WriteLine();
+                Console.WriteLine("{0} {1} {2}", numberOfCupsOfCoffee, fullName, today);
+                    Console.WriteLine();
+            
+            
             Console.WriteLine("Hey friend, what's your name?");
-            Console.Write("");
+                var name = Console.ReadLine();
+                    Console.WriteLine();
+            
+                if (name == "Alice")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("In that direction, <-- Alice, lives a Hatter, and in that --> direction lives a March Hare. Visit either you like: they’re both mad.");
+                }
+                else if (name == "alice") 
+                {
+                    Console.WriteLine("In that direction, <-- Alice, lives a Hatter, and in that --> direction lives a March Hare. Visit either you like: they’re both mad.");
+                }
+                else
+                {
+                    Console.WriteLine($"It is a pleasure to meet you, {name}.");
+                }
+            
             Console.WriteLine();
-            var name = Console.ReadLine();
-            if (name == "Alice")
-            {
-                Console.WriteLine();
-
-                Console.WriteLine("In that direction, <-- Alice, lives a Hatter, and in that --> direction lives a March Hare. Visit either you like: they’re both mad.");
-            }
-            else if (name == "alice")
-            {
-                Console.WriteLine("In that direction, <-- Alice, lives a Hatter, and in that --> direction lives a March Hare. Visit either you like: they’re both mad.");
-            }
-            else
-            {
-                Console.WriteLine($"It is a pleasure to meet you, {name}");
-            }
-            Console.WriteLine();
+            
             Console.WriteLine($"Please input a number, {name}.");
             var firstNumberAsString = Console.ReadLine();
+            
+            Console.WriteLine();
+            
             Console.WriteLine("Please input another number.");
             var secondNumberAsString = Console.ReadLine();
+                    
             double firstOperand = double.Parse(firstNumberAsString);
             double secondOperand = double.Parse(secondNumberAsString);
+            
             Console.WriteLine();
+            
             double sum = firstOperand + secondOperand;
             double difference = firstOperand - secondOperand;
             double product = firstOperand * secondOperand;
             double quotient = firstOperand / secondOperand;
             double remainder = firstOperand % secondOperand;
+            
             Console.WriteLine($"Adding {firstNumberAsString} to {secondNumberAsString} = {sum}.");
             Console.WriteLine();
             Console.WriteLine($"Subtracting {firstNumberAsString} from {secondNumberAsString} = {difference}.");
@@ -60,6 +70,7 @@ namespace VariablesCS
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
+            
         }
     }
 }
